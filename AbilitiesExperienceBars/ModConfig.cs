fc211eed805d2d5b1f8454c94541b0f0b9297d61
Gustavo Hinkel <78951385+Ophaneom@users.noreply.github.com>
2021-03-06 12:07:51 -0300

    Changed archives location

---
+++ AbilitiesExperienceBars/ModConfig.cs
@@ -0,0 +1,23 @@
+﻿using StardewModdingAPI;
+
+namespace AbilitiesExperienceBars
+{
+    public class ModConfig
+    {
+        public SButton ToggleKey { get; set; }
+        public SButton ConfigKey { get; set; }
+        public SButton ResetKey { get; set; }
+
+        public bool ShowButtons { get; set; }
+        public bool ShowExperienceInfo { get; set; }
+        public bool ShowBoxBackground { get; set; }
+        public bool ShowLevelUp { get; set; }
+        public bool ShowUI { get; set; }
+
+        public float LevelUpMessageDuration { get; set; }
+
+        public int mainPosX { get; set; }
+        public int mainPosY { get; set; }
+        public int mainScale { get; set; }
+    }
+}
